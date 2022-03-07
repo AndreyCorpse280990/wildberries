b436652d1cf1791cbdb16f4064c0a2f722abda9e
AndreyBukov <280990corpse@gmail.com>
2022-03-07 16:49:29 +0300

    разбил по функциям

--- parser.py
+++ parser.py
@@ -1,28 +1,30 @@
+from pydoc import text
 import requests
-import json
+import logging
 from bs4 import BeautifulSoup
+
+logging.basicConfig(filename="parser.log", level=logging.INFO)
 
 url = 'https://www.wildberries.ru/catalog/19257508/detail.aspx?targetUrl=XS'
 
 def description(url):
     try:
         responce = requests.get(url)
-        print(responce.status_code)
+        return responce.text
     except(requests.RecursionError, ConnectionResetError, requests.RequestException):
             print('Ошибка сети')
             return False
-    soup = BeautifulSoup(responce.text, 'lxml')
-    parameters = soup.find_all('body', class_='ru')
-    for i in parameters:
-        param = {}
-        param = i.find('script').text
-        a = param.find('{')
-        b = param.rfind('}')
-        param = param[a:b+1]
-        print(param)
-        json_param = json.loads(param)
-        print(json_param)
- 
+    
+def get_information(html):
+    soup = BeautifulSoup(html, 'lxml')
+    info = soup.find('div', {'id': 'app'}).find(id)
+    app = info.find('meta', {'itemprop': 'name'}) # тут незаконченный вопрос с name
+    print(app)
+    
+    
+def main(url):
+    html = description(url)
+    get_information(html)
         
 if __name__=="__main__":
-    description('https://www.wildberries.ru/catalog/19257508/detail.aspx?targetUrl=XS')
+    main(url)
