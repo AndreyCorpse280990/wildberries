e8159d6e72edf0af649be08402d0899e3a562341
AndreyBukov <280990corpse@gmail.com>
2022-03-13 15:31:29 +0300

    write_file

--- selenium_similar.py
+++ selenium_similar.py
@@ -1,8 +1,9 @@
 import requests
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.keys import Keys
 
 def get_html(url):
     s=Service(
@@ -17,14 +18,21 @@ def get_html(url):
 
     try:
         driver.get(url=url)
-        time.sleep(5)
-        
+        time.sleep(3)
+        html = driver.find_element_by_class_name('pagination-next')
+
         while True:
+            html.send_keys(Keys.END)
+            time.sleep(3)
             product_card = driver.find_element_by_xpath("//div[@id='catalog-content']")
-            with open('test.html', 'w') as file:
-                file.write(str(product_card))
-                
-            print(product_card.text)
+            #js = json.loads(product_card)
+            #print(js)
+            product_text = product_card.text
+
+            with open('test.html', 'w', encoding="utf-8") as file:
+                file.write(product_text)
+            
+            print(product_text)
             break
     except Exception as ex:
         print(ex)
