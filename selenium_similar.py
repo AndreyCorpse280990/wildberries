cd5d2e57e852f55aa94f4b57a466e314eccc3b4b
AndreyBukov <280990corpse@gmail.com>
2022-03-20 14:27:53 +0300

    added antibot

--- selenium_similar.py
+++ selenium_similar.py
@@ -1,47 +1,71 @@
 import csv
 import bs4
-import requests
 import time
-import json
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium_stealth import stealth
 from selenium.webdriver.common.keys import Keys
-from operator import methodcaller
+
 
 def get_html(url):
-    s=Service(
-        'C:\\Users\\corps\\Desktop\\Програмирование\\Learn Python\\project\\wildberries\\chromedriver\\chromedriver.exe'
-        )
-    
-    
-    driver = webdriver.Chrome(service=s)
-    driver.maximize_window()
-    
-    
-
     try:
+        # to protect against detection
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized")
+
+        options.add_experimental_option("excludeSwitches",
+                                        ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        driver = webdriver.Chrome(options=options,
+                                  executable_path=r"chromedriver\\chromedriver.exe")
+
+        # client settings
+
+        stealth(driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win64",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,
+                )
         driver.get(url=url)
-        time.sleep(3)
-        html = driver.find_element_by_class_name('pagination-next')
+        time.sleep(11)
+        html = driver.find_element_by_class_name('nav-element__logo')
+
+
 
         while True:
-            html.send_keys(Keys.END)
+            html.send_keys(Keys.PAGE_DOWN) * 5
             time.sleep(3)
-            product_card = driver.find_element_by_xpath("//div[@id='catalog-content']")
-            soup = bs4.BeautifulSoup(product_card.get_attribute('innerHTML'), 'lxml')
+            # searching for a block of elements and getting html
+            product_card = driver.find_element_by_xpath(
+                "//div[@id='catalog-content']")
+            soup = bs4.BeautifulSoup(product_card.get_attribute('innerHTML'),
+                                     'lxml')
+
+            # getting the text of products and prices
             find_price = soup.find_all('span', {'class': 'lower-price'})
-            lower_price = ([price.get_text() for price in find_price])
-            
+            lower_price = ([price.get_text().strip() for price in find_price])
             find_name = soup.find_all('span', {'class': 'goods-name'})
             goods_name = ([name.get_text() for name in find_name])
             
+            #for n, p in zip(goods_name, lower_price):
+                #print(n)
+                #print(p)
+                
+            path_name = 'C:\\Users\\corps\\Desktop\\Програмирование\\Learn Python\\project\\wildberries\\name.csv'
+            with open(path_name, 'w', encoding='utf-8') as file_name:
+                writer_name = csv.writer(file_name, delimiter=':')
+                writer_name.writerows([goods_name])
+
+            path_price = 'C:\\Users\\corps\\Desktop\\Програмирование\\' \
+                         'Learn Python\\project\\wildberries\\price.csv'
+            with open(path_price, 'w', encoding='utf-8') as file_price:
+                writer_price = csv.writer(file_price, delimiter=';')
+                writer_price.writerows([lower_price])
             
-            print(lower_price)
-            print(goods_name)
-            #with open('test.txt', 'w', encoding="utf-8") as file:
-                #file.write(" ".join(lower_price))
-             #   file.write(goods_name.split(','))#непонятно как разделить кроме как циклом for
-             #   file.write(map(goods_name.split(',')))
+            print(soup.text)
+            
             break
     except Exception as ex:
         print(ex)
@@ -49,8 +73,11 @@ def get_html(url):
         driver.close()
         driver.quit()
 
+
 def main():
-    get_html(url='https://www.wildberries.ru/catalog/0/search.aspx?search=hdd+3.5')
+    get_html(
+        url='https://www.wildberries.ru/catalog/0/search.aspx?search=hdd+3.5')
+
 
 if __name__ == '__main__':
     main()
