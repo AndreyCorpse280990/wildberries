b45a0d9cd2bf55edc66fec259fdb3d20b9232c76
AndreyBukov <280990corpse@gmail.com>
2022-03-31 14:58:22 +0300

    edit lower_prise in int

--- webapp/saving_to_database.py
+++ webapp/saving_to_database.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+import bcrypt
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-#from config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -15,25 +15,33 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
+    def encryption_password(self, password):
+        pwhash = bcrypt.hashpw(password, self.password)
+        return self.password == pwhash
+
+    if user.encryption_password(
+            login_data['password']):  # не понял откуда берётся login_data
+        print('Access granted')
+
     def __repr__(self):
         return '<User %r>' % self.username
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String, nullable=False)
     item = db.Column(db.String(200), nullable=False)
     url = db.Column(db.String(200), unique=True)
     item_id = relationship("ItemPrice")
 
     def __repr__(self):
         return 'Item %r' % self.name
-    
+
 
 class ItemPrice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
     item = db.Column(db.String(200), nullable=False)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+
     def __repr__(self):
         return '<Category %r>' % self.name
