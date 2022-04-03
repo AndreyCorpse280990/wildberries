ee505a9d826ba8249f579573399d7675b6684da4
AndreyBukov <280990corpse@gmail.com>
2022-04-03 20:00:30 +0300

    test

---
+++ get_all_info.py
@@ -0,0 +1,8 @@
+from webapp import create_app
+# from webapp.selenium_similar import get_html
+from webapp import config
+from webapp.import_database import save_to_base
+
+app = create_app()
+with app.app_context():
+    save_to_base(config.URL_SELENIUM)
