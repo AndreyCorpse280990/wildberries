de25d6556416fea14ac924fd651f48329d5afb15
AndreyBukov <280990corpse@gmail.com>
2022-04-04 12:29:07 +0300

    test

--- webapp/saving_to_database.py
+++ webapp/saving_to_database.py
@@ -1,12 +1,6 @@
-import bcrypt
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from webapp import config
 from sqlalchemy.orm import relationship
-from db import Base, engine
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-app.config['SQLALCHEMY_DATABASE_URI']
-db = SQLAlchemy(app)
+from webapp.import_database import *
 
 
 class User(db.Model):
@@ -21,22 +15,20 @@ class User(db.Model):
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
     url = db.Column(db.String(200), unique=True)
 
     def __repr__(self):
-        return f"Item {self.id}, {self.item}"
+        return f"Item {self.id}, {self.name}"
 
 
 class ItemPrice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-    item = relationship('Item')
+    name = db.Column(db.String(200), nullable=False)
+    #name = db.relationship('Item')
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
 
     def __repr__(self):
-        return f"ItemPrice {self.id}, {self.item}"
+        return f"ItemPrice {self.id}, {self.name}"
 
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
