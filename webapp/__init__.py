c0db86b91dcbc2a93de5dbb141ebb57edb6cdf43
AndreyBukov <280990corpse@gmail.com>
2022-04-05 08:37:19 +0300

    added added a registration form

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 from webapp.saving_to_database import db
+from webapp.forms import LoginForm
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
+
+    @app.route('/login')
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
     return app
