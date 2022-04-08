9e797682ab17cef4c7db185a0ee1ab55f35d8046
AndreyBukov <280990corpse@gmail.com>
2022-04-08 11:41:42 +0300

    добавил blueprint

---
+++ webapp/User/models.py
@@ -0,0 +1,27 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from webapp.saving_to_database import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), index=True, unique=True,
+                         nullable=False)
+    # Добавить в будущем.
+    #email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == "admin"
+
+    def __repr__(self):
+        return f"User {self.id}, {self.username}"
--- webapp/saving_to_database.py
+++ webapp/saving_to_database.py
@@ -1,32 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from webapp import config
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy()
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), index=True, unique=True,
-                         nullable=False)
-    # Добавить в будущем.
-    #email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    role = db.Column(db.String(10), index=True)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    def __repr__(self):
-        return f"User {self.id}, {self.username}"
 
 
 class Item(db.Model):
