08cbaa098dfcfe5e6ac3bc27efdcce9e256178b6
AndreyCorpse280990 <280990corpse@gmail.com>
2023-01-14 19:42:40 +0300

    refactoring

--- create_db.py
+++ create_db.py
@@ -1,11 +1,3 @@
 from webapp import db, create_app
-<<<<<<< HEAD
-from flask import send_from_directory
-
-app = create_app()
-db.init_app(app)
-db.create_all(app=app)
-=======
 
 db.create_all(app=create_app())
->>>>>>> main
